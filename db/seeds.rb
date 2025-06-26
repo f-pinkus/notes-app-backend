dbfb7e30274c3940f0572961f8cc0c3a8d8d09df
f-pinkus <faigypinkus@gmail.com>
2025-06-26 16:32:22 -0400

    create note model and seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,4 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Note.create(title: "Doc Appointment", body: "Monday at 3:15")
+Note.create(title: "Supper Menu", body: "Chicken cutlets, roasted potatoes, caesar salad")
+Note.create(title: "To Buy", body: "shampoo, body wash, lens solution, vaseline")
+Note.create(title: "Don't Forget!", body: "Call Mom - birthday tomorrow")
