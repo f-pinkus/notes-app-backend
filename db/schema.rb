e4039b40d3f80270927a256a95dafd28fa31403d
f-pinkus <faigypinkus@gmail.com>
2025-07-04 10:02:33 -0400

    add notes associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_26_215027) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_26_221214) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -19,6 +19,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_26_215027) do
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_notes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -28,4 +30,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_26_215027) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "notes", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-Note.create(title: "Doc Appointment", body: "Monday at 3:15")
-Note.create(title: "Supper Menu", body: "Chicken cutlets, roasted potatoes, caesar salad")
-Note.create(title: "To Buy", body: "shampoo, body wash, lens solution, vaseline")
-Note.create(title: "Don't Forget!", body: "Call Mom - birthday tomorrow")
+Note.create(title: "Doc Appointment", body: "Monday at 3:15", user_id: 2)
+Note.create(title: "Supper Menu", body: "Chicken cutlets, roasted potatoes, caesar salad", user_id: 1)
+Note.create(title: "To Buy", body: "shampoo, body wash, lens solution, vaseline", user_id: 1)
+Note.create(title: "Don't Forget!", body: "Call Mom - birthday tomorrow", user_id: 2)
